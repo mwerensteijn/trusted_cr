1d3acc9d036ede082dc16f0d64079a95ee27ed8d
mwerensteijn <mitchellwerensteijn@msn.com>
2020-09-07 19:48:43 +0200

    Set the environment variables to build the project

---
+++ setup_build_envs.sh
@@ -0,0 +1,4 @@
+export CROSS_COMPILE="/opt/optee-qemu/toolchains/aarch64/bin/aarch64-linux-gnu-"
+export HOST_CROSS_COMPILE="/opt/optee-qemu/toolchains/aarch64/bin/aarch64-linux-gnu-"
+export TEEC_EXPORT="/opt/optee-qemu/optee_client/out/export/usr"
+export TA_DEV_KIT_DIR="/opt/optee-qemu/optee_os/out/arm/export-ta_arm64"
