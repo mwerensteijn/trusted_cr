4f3e1dc4a5dd1bc10fab16ee73d3485c236b63ea
mwerensteijn <mitchellwerensteijn@msn.com>
2020-09-08 08:20:51 +0200

    Scripts to ease testing

---
+++ install_and_run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cp -rf ./*.ta /lib/optee_armtz/
+./optee_app_migrator
--- push_binaries.sh
+++ push_binaries.sh
@@ -2,4 +2,4 @@
 
 cp -rf ta/*.ta /opt/optee-qemu/build/shared_folder/
 cp -rf host/optee_app_migrator /opt/optee-qemu/build/shared_folder/
-
+cp -rf install_and_run.sh /opt/optee-qemu/build/shared_folder/
