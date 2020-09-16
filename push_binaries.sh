a12679be6cceef90f09bc5673de5ecec3cbbf1cf
mwerensteijn <mitchellwerensteijn@msn.com>
2020-09-16 11:56:56 +0200

    Copy to clipboard

--- push_binaries.sh
+++ push_binaries.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "mkdir shared && mount -t 9p -o trans=virtio host shared && cd shared" | xclip -sel clip
 cp -rf ta/*.ta /opt/optee-qemu/build/shared_folder/
 cp -rf host/optee_app_migrator /opt/optee-qemu/build/shared_folder/
 cp -rf install_and_run.sh /opt/optee-qemu/build/shared_folder/
--- start_qemu.sh
+++ start_qemu.sh
@@ -1,4 +1,5 @@
 source /opt/optee-qemu/optee_examples/optee_app_migrator/setup_build_envs.sh
 echo "Run this: mkdir shared && mount -t 9p -o trans=virtio host shared"
 cd /opt/optee-qemu/build
-make run-only QEMU_VIRTFS_ENABLE=y QEMU_VIRTFS_HOST_DIR=$(pwd)/shared_folder
+./run.sh
+
