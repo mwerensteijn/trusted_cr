30e9a8df95845b198f5c7996dd935f042db292aa
Mitchell Werensteijn <mitchellwerensteijn@msn.com>
2021-01-02 14:25:06 +0100

    Starting putting functions in their own file

---
+++ host/encoding.h
@@ -0,0 +1,296 @@
+#ifndef ENCODING_H
+#define ENCODING_H
+
+#include "criu/criu_checkpoint.h"
+#include "criu/criu_checkpoint_parser.h"
+
+void fprintf_substring(FILE * file, char * buffer, int start_index, int end_index) {
+	char backup = buffer[end_index];
+	buffer[end_index] = 0;
+	fprintf(file, buffer + start_index);
+	buffer[end_index] = backup;	
+}
+
+void write_updated_core_checkpoint(char * new_filename, void * buffer, long file_size, struct criu_checkpoint * checkpoint) {
+	FILE *fpp = fopen(new_filename, "w+");
+	if(fpp) {
+		// Initialize the JSMN json parser
+		jsmn_parser parser;
+		jsmn_init(&parser);
+
+		// First only determine the number of tokens.
+		int items = jsmn_parse(&parser, buffer, file_size, NULL, 128);
+
+		jsmntok_t tokens[items];
+		
+		// Reset position in stream
+		jsmn_init(&parser);
+		int left = jsmn_parse(&parser, buffer, file_size, tokens, items);
+
+		// Invalid file.
+		if (items < 1 || tokens[0].type != JSMN_OBJECT) {
+			printf("CRIU: INVALID JSON\n");
+			return -1;
+		}
+
+		// TLS indexes
+		int before_tls_value = 0;
+		int  after_tls_value = 0;
+
+		// Regs indexes
+		int before_regs_value = 0;
+		int  after_regs_value = 0;
+
+		// SP indexes
+		int before_sp_value = 0;
+		int  after_sp_value = 0;
+
+		// PC indexes
+		int before_pc_value = 0;
+		int  after_pc_value = 0;
+
+		// Pstate indexes
+		int before_pstate_value = 0;
+		int  after_pstate_value = 0;
+
+		// Vregs indexes
+		int before_vregs_value = 0;
+		int  after_vregs_value = 0;
+
+		// FPSR indexes
+		int before_fpsr_value = 0;
+		int  after_fpsr_value = 0;
+
+		// FPCR indexes
+		int before_fpcr_value = 0;
+		int  after_fpcr_value = 0;
+
+		// Parse the JSON version of the core checkpoint file (example core-2956.img)
+		for(int i = 1; i < items; i++) {
+			if (jsoneq(buffer, &tokens[i], "tls") == 0) { 
+				before_tls_value = tokens[i+1].start;
+				after_tls_value = tokens[i+1].end;
+			} else if (jsoneq(buffer, &tokens[i], "regs") == 0) { 
+				before_regs_value = tokens[i+1].start;
+				after_regs_value = tokens[i+1].end;
+			} else if (jsoneq(buffer, &tokens[i], "sp") == 0) {
+				before_sp_value = tokens[i+1].start;
+				after_sp_value = tokens[i+1].end;
+			} else if (jsoneq(buffer, &tokens[i], "pc") == 0) {
+				before_pc_value = tokens[i+1].start;
+				after_pc_value = tokens[i+1].end;
+			} else if (jsoneq(buffer, &tokens[i], "pstate") == 0) {
+				before_pstate_value = tokens[i+1].start;
+				after_pstate_value = tokens[i+1].end;
+			} else if (jsoneq(buffer, &tokens[i], "vregs") == 0) {
+				before_vregs_value = tokens[i+1].start;
+				after_vregs_value = tokens[i+1].end;
+			} else if (jsoneq(buffer, &tokens[i], "fpsr") == 0) {
+				before_fpsr_value = tokens[i+1].start;
+				after_fpsr_value = tokens[i+1].end;
+			} else if (jsoneq(buffer, &tokens[i], "fpcr") == 0) {
+				before_fpcr_value = tokens[i+1].start;
+				after_fpcr_value = tokens[i+1].end;
+			}
+		}
+
+		fprintf_substring(fpp, buffer, 0, before_tls_value);
+
+		// Write tpidr_el0
+		fprintf(fpp, "%llu", checkpoint->regs.tpidr_el0_addr); 
+		fprintf_substring(fpp, buffer, after_tls_value, before_regs_value);
+
+		// Write all updated registers
+		fputc('[', fpp);
+		for(int i = 0; i < 31; i++) {
+			fprintf(fpp, "\"0x%lx\"", checkpoint->regs.regs[i]);
+			
+			if(i != 30)
+				fprintf(fpp, ",\n");
+		}
+		fputc(']', fpp);
+		fprintf_substring(fpp, buffer, after_regs_value, before_sp_value);
+
+		// Write updated stack pointer
+		fprintf(fpp, "0x%lx", checkpoint->regs.stack_addr);
+		fprintf_substring(fpp, buffer, after_sp_value, before_pc_value);
+
+		// Write updated program counter
+		fprintf(fpp, "0x%lx", checkpoint->regs.entry_addr);
+		fprintf_substring(fpp, buffer, after_pc_value, before_pstate_value);
+		
+		// Write updated pstate
+		fprintf(fpp, "0x%lx", checkpoint->regs.pstate);
+		fprintf_substring(fpp, buffer, after_pstate_value, before_vregs_value);
+
+		// Write all updated vregs
+		fputc('[', fpp);
+		for(int i = 0; i < 64; i++) {
+			fprintf(fpp, "%llu", checkpoint->regs.vregs[i]);
+
+			if(i != 63)
+				fprintf(fpp, ",\n");
+		}
+		fputc(']', fpp);
+		fprintf_substring(fpp, buffer, after_vregs_value, before_fpsr_value);
+
+		// Write updated fpsr
+		fprintf(fpp, "%lu", checkpoint->regs.fpsr);
+		fprintf_substring(fpp, buffer, after_fpsr_value, before_fpcr_value);
+		
+		// Write updated fpcr
+		fprintf(fpp, "%lu", checkpoint->regs.fpcr);
+		fprintf_substring(fpp, buffer, after_fpcr_value, file_size);
+
+		fclose(fpp);
+	}
+}
+
+void write_updated_pages_checkpoint(struct criu_merged_pagemap * merged_map, struct criu_dirty_page * dirty_page, int dirty_page_count, void * page_data, char * original_buffer) {
+	FILE *fpages = fopen("modified_pages-1.img", "w+");
+	if(fpages) {
+		struct criu_merged_page * entry = NULL;
+		TAILQ_FOREACH(entry, merged_map, link) {
+			for(int i = 0; i < entry->entry.nr_pages; i++) {
+				vaddr_t addr = entry->entry.vaddr_start + i * 4096;
+	
+				bool dirty = false;
+				for(int y = 0; y < dirty_page_count; y++) {
+					if(addr == dirty_page[y].vaddr_start) {
+						// Write dirty page
+						fwrite(page_data + y * 4096, 1, 4096, fpages);
+						// printf("dirty page at: %p - index: %d\n", addr, entry->entry.file_page_index + i);
+						dirty = true;
+						break;
+					}
+				}
+
+				if(!dirty) {
+					// Write the original
+					fwrite(original_buffer + (entry->entry.file_page_index + i) * 4096, 1, 4096, fpages);
+					// printf("clean page at: %p - index: %d\n", addr, entry->entry.file_page_index + i);
+				}
+			}
+		}
+
+		fclose(fpages);
+	} else {
+		printf("Unable to open pages-1.img for writing..");
+	}
+}
+
+void write_updated_pagemap_checkpoint(struct criu_merged_pagemap * merged_pagemap, char * buffer, long file_size) {
+	FILE *fpagemap = fopen("modified_pagemap.txt", "w+");
+
+	if(fpagemap) {
+		// Initialize the JSMN json parser
+		jsmn_parser parser;
+		jsmn_init(&parser);
+
+		// First only determine the number of tokens.
+		int items = jsmn_parse(&parser, buffer, file_size, NULL, 128);
+
+		jsmntok_t tokens[items];
+		
+		// Reset position in stream
+		jsmn_init(&parser);
+		int left = jsmn_parse(&parser, buffer, file_size, tokens, items);
+
+		// Invalid file.
+		if (items < 1 || tokens[0].type != JSMN_OBJECT) {
+			printf("CRIU: INVALID JSON\n");
+			return -1;
+		}
+
+		// First vaddr index
+		int vaddr_start = 0;
+
+		// Parse the JSON version of the core checkpoint file (example core-2956.img)
+		for(int i = 1; i < items; i++) {
+			if (jsoneq(buffer, &tokens[i], "vaddr") == 0) { 
+				vaddr_start = tokens[i-1].start;
+				break;
+			}
+		}
+
+		char backup = buffer[vaddr_start];
+		buffer[vaddr_start] = 0;
+		fprintf(fpagemap, "%s", buffer);
+		buffer[vaddr_start] = backup;	
+
+		struct criu_merged_page * entry = NULL;
+		TAILQ_FOREACH(entry, merged_pagemap, link) {
+			fprintf(fpagemap, "{\n\t\"vaddr\": \"0x%lx\",\n\t\"nr_pages\": %d,\n\t\"flags\": \"", entry->entry.vaddr_start, entry->entry.nr_pages);
+			
+			bool require_seperator = false;
+			if(entry->entry.flags & PE_LAZY) {
+				fprintf(fpagemap, "PE_LAZY");
+				require_seperator = true;
+			}
+
+			if(entry->entry.flags & PE_PRESENT) {
+				if(require_seperator)
+					fprintf(fpagemap, " | ");
+
+				fprintf(fpagemap, "PE_PRESENT");
+			}
+
+			fprintf(fpagemap, "\"\n}");
+
+			if(entry->link.tqe_next != NULL)
+				fprintf(fpagemap, ",\n");
+		}
+
+		fprintf(fpagemap, "] }");
+
+		fclose(fpagemap);
+	} else {
+		printf("Unable to open modified_pagemap.txt");
+	}
+}
+
+
+void create_merged_map(struct criu_merged_pagemap * merged_map, struct criu_checkpoint * checkpoint, 
+					   struct criu_dirty_page * dirty_entry, int dirty_page_count) {
+	for(int i = 0; i < checkpoint->pagemap_entry_count; i++) {
+		struct criu_merged_page * new_entry = calloc(1, sizeof(struct criu_merged_page));
+		new_entry->is_new = false;
+		memcpy(&new_entry->entry, &checkpoint->pagemap_entries[i], sizeof(struct criu_pagemap_entry));
+
+		TAILQ_INSERT_TAIL(merged_map, new_entry, link);
+	}
+
+	for(int i = 0; i < dirty_page_count; i++) {
+		bool skip = false;
+		bool insert_before = false;
+		struct criu_merged_page * entry = NULL;
+		TAILQ_FOREACH(entry, merged_map, link) {
+			if((entry->entry.vaddr_start <= dirty_entry[i].vaddr_start)  &&
+			(dirty_entry[i].vaddr_start < (entry->entry.vaddr_start + entry->entry.nr_pages * 4096))) {
+				skip = true;
+				break;
+			} else if(dirty_entry[i].vaddr_start < entry->entry.vaddr_start) {
+				insert_before = true;
+				break;
+			}
+		}		
+
+		if(skip)
+			continue;
+
+		struct criu_merged_page * new_entry = calloc(1, sizeof(struct criu_merged_page));
+		
+		new_entry->entry.vaddr_start = dirty_entry[i].vaddr_start;
+		new_entry->entry.nr_pages = 1;
+		new_entry->entry.file_page_index = -1;
+		new_entry->entry.flags = PE_LAZY | PE_PRESENT;
+		new_entry->is_new = true;
+
+		if(insert_before)
+			TAILQ_INSERT_BEFORE(entry, new_entry, link);
+		else
+			TAILQ_INSERT_TAIL(merged_map, new_entry, link);
+	}
+}
+
+#endif /* ENCODING_H */
---
+++ host/file_handling.h
@@ -0,0 +1,60 @@
+#ifndef FILE_HANDLING_H
+#define FILE_HANDLING_H
+
+#include "criu/criu_checkpoint.h"
+#include "criu/criu_checkpoint_parser.h"
+
+// TODO: do this differently, it is now duplicate in main.c and in here.
+#define CHECKPOINT_FILES 7 
+#define CHECKPOINT_FILENAME_MAXLENGTH 100
+
+bool read_file(struct checkpoint_file_data * c_file) {
+	FILE *f = fopen(c_file->filename, "rb");
+
+	if(f) {
+		// Determine file size
+		fseek(f, 0, SEEK_END);
+		c_file->file.file_size = ftell(f);
+		fseek(f, 0, SEEK_SET);
+
+		c_file->buffer = malloc(c_file->file.file_size + 1);
+
+		if(c_file->buffer) {
+			fread(c_file->buffer, 1, c_file->file.file_size, f);
+			c_file->buffer[c_file->file.file_size] = 0;
+		} else {
+			// Unable to malloc.
+			printf("Unable to malloc %ld bytes for file %s.\n", c_file->file.file_size, c_file->filename);
+			c_file->file.file_size = -1;
+			return false;
+		}
+
+		fclose(f);
+	} else {
+		printf("Unable to read file: %s\n", c_file->filename);
+		return false;
+	}
+
+	return true;
+}
+
+void read_checkpoint_files(int pid, struct checkpoint_file_data * files) {
+	char filenames[CHECKPOINT_FILES][CHECKPOINT_FILENAME_MAXLENGTH] = {};
+
+	snprintf(filenames[CORE_FILE], CHECKPOINT_FILENAME_MAXLENGTH, "core-%d.txt", pid);
+	snprintf(filenames[MM_FILE], CHECKPOINT_FILENAME_MAXLENGTH, "mm-%d.txt", pid);
+	snprintf(filenames[PAGEMAP_FILE], CHECKPOINT_FILENAME_MAXLENGTH, "pagemap-%d.txt", pid);
+	snprintf(filenames[FILES_FILE], CHECKPOINT_FILENAME_MAXLENGTH, "files.txt");
+	snprintf(filenames[PAGES_BINARY_FILE], CHECKPOINT_FILENAME_MAXLENGTH, "pages-1.img");
+	
+	// Skip the last EXECUTABLE_BINARY_FILE, because we do not know the executable name yet which is parsed from FILES_FILE
+	for(int i = PAGES_BINARY_FILE; i <= FILES_FILE; i++) {
+		// Set the filename, load the filesize and read the file from disk into the buffer
+		files[i].filename = filenames[i];
+		read_file(&files[i]);
+
+		// printf("%d: %s - size: %d\n", i, filenames[i], files[i].file.file_size);
+	}
+}
+
+#endif /* FILE_HANDLING_H */
